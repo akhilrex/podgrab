f0c571d0719395b6facae43cdf71f750a9e3e91f
Arno Hautala <arno@hautala.dev>
2021-06-03 22:49:09 -0400

    replaced appending date and episode number with file format in Setting struct, SettingModel struct, UpdateSetting()

--- controllers/podcast.go
+++ controllers/podcast.go
@@ -463,9 +463,14 @@ func UpdateSetting(c *gin.Context) {
 
 	if err == nil {
 
-		err = service.UpdateSettings(model.DownloadOnAdd, model.InitialDownloadCount,
-			model.AutoDownload, model.AppendDateToFileName, model.AppendEpisodeNumberToFileName,
-			model.DarkMode, model.DownloadEpisodeImages, model.GenerateNFOFile)
+		err = service.UpdateSettings(
+			model.DownloadOnAdd,
+			model.InitialDownloadCount,
+			model.AutoDownload,
+			model.FileNameFormat,
+			model.DarkMode,
+			model.DownloadEpisodeImages,
+			model.GenerateNFOFile)
 		if err == nil {
 			c.JSON(200, gin.H{"message": "Success"})
 
