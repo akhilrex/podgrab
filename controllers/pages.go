822ec69f12f9d6ef1d9ec9e79f705738a95a77b3
James Luck <james.luck@thetradedesk.com>
2022-06-11 15:01:12 +1000

    Fixing type of setting and formatting

--- controllers/pages.go
+++ controllers/pages.go
@@ -31,7 +31,7 @@ type SettingModel struct {
 	GenerateNFOFile               bool   `form:"generateNFOFile" json:"generateNFOFile" query:"generateNFOFile"`
 	DontDownloadDeletedFromDisk   bool   `form:"dontDownloadDeletedFromDisk" json:"dontDownloadDeletedFromDisk" query:"dontDownloadDeletedFromDisk"`
 	BaseUrl                       string `form:"baseUrl" json:"baseUrl" query:"baseUrl"`
-	MaxDownloadConcurrency        bool   `form:"maxDownloadConcurrency" json:"maxDownloadConcurrency" query:"maxDownloadConcurrency"`
+	MaxDownloadConcurrency        int    `form:"maxDownloadConcurrency" json:"maxDownloadConcurrency" query:"maxDownloadConcurrency"`
 }
 
 var searchOptions = map[string]string{
